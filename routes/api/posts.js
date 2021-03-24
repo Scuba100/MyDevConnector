1da02ef3e030654a5067408c4e5be78f6f14b7fd
Scuba100 <ascubadiver100@gmail.com>
2021-03-23 23:15:07 -0500

    Initial commitconfig/ 	git.ignore 	middleware/ 	models/ 	node_modules/ 	package-lock.json 	package.json 	routes/ 	server.js

---
+++ routes/api/posts.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   POST api/posts
+// @desc    Register user
+// @access  Public
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Posts route');
+});
+
+module.exports = router;
