d6cc263b7a6444c4baf21a23ace36d76eccdeefb
Scuba100 <ascubadiver100@gmail.com>
2021-04-08 02:51:50 -0500

    axios added to npm

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import './App.css';
